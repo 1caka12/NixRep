87787343fead0e5bbddfc7fd1b4ea1ce2c2cc18c
Ivo <i.matsaliev@student.vu.nl>
2024-09-28 11:17:48 +0200

    nvim still does not work

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -32,20 +32,37 @@
     userName = "Ivo";
     userEmail = "i.matsaliev@student.vu.nl";
   };
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [
-	nvim-lspconfig
-	nvim-treesitter.withAllGrammars
-	catppuccin-nvim
-	nvim-cmp
-	telescope-nvim
-    ];
-  };
   programs.fish = {
     enable = true;
   };
+ programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+
+    # Plugins managed by Home Manager (Nix)
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig          # LSP plugin
+      nvim-treesitter          # Treesitter for syntax highlighting
+      telescope-nvim           # Telescope for fuzzy finding
+    ];
+
+    # Neovim Lua configuration
+    extraConfig = ''
+      -- Treesitter configuration
+      require('nvim-treesitter.configs').setup {
+        highlight = {
+          enable = true,      -- Enable syntax highlighting
+        },
+      }
+
+      -- Telescope setup
+      require('telescope').setup{}
+
+      -- Keybindings for Telescope
+      vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>Telescope find_files<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>Telescope live_grep<CR>]], { noremap = true, silent = true })
+    '';
+  };  
   programs.firefox.enable = true;
   programs.starship = {
     enable = true;
