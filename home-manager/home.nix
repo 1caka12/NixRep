9c70fc2c2d322765752f69b55bfed68b00826d29
Ivo <i.matsaliev@student.vu.nl>
2024-09-28 01:09:35 +0000

    Back to kitty

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -50,15 +50,6 @@
     };
   };
 
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      env.TERM = "xterm-256color";
-     font = {
-       size = 12;
-       draw_bold_text_with_bright_colors = true;
-     };
-   };
-  };
+  programs.kitty.enable = true;
   programs.home-manager.enable = true;
 }
