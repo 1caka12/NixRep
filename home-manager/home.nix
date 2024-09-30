26bdc057c6bd5390c71331a706b16ed43e1ece76
Ivo <i.matsaliev@student.vu.nl>
2024-09-30 05:52:14 +0200

    Add tmux

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,7 +22,6 @@
     networkmanager
     nerdfonts
     fzf
-    tmux
     pavucontrol
   ];
 
@@ -36,7 +35,20 @@
   };
   programs.fish = {
     enable = true;
-
+  };
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs;[
+        tmuxPlugins.catppuccin
+        tmuxPlugins.battery
+        tmuxPlugins.better-mouse-mode
+    ];
+    extraConfig = "
+        unbind C-b
+        set-option -g prefix C-Space
+        bind-key C-Space send-prefix
+        set -g status-position top
+    ";
   };
   programs.firefox.enable = true;
   programs.starship = {
@@ -84,8 +96,64 @@
             };
        };
        lsp = {
-	   servers.clangd.enable = true;
+           servers.clangd.enable = true;
        };     
+
+           cmp-emoji = {enable = true;};
+        cmp = {
+          enable = true;
+          settings = {
+            autoEnableSources = true;
+            experimental = {ghost_text = false;};
+            performance = {
+              debounce = 60;
+              fetchingTimeout = 200;
+              maxViewEntries = 30;
+            };
+            snippet = {expand = "luasnip";};
+            formatting = {fields = ["kind" "abbr" "menu"];};
+            sources = [
+              {name = "git";}
+              {name = "nvim_lsp";}
+              {name = "emoji";}
+              {
+                name = "buffer"; # text within current buffer
+                option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+                keywordLength = 3;
+              }
+              {
+                name = "path"; # file system paths
+                keywordLength = 3;
+              }
+              {
+                name = "luasnip"; # snippets
+                keywordLength = 3;
+              }
+            ];
+
+            window = {
+              completion = {border = "solid";};
+              documentation = {border = "solid";};
+            };
+
+            mapping = {
+              "<C-Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+              "<C-j>" = "cmp.mapping.select_next_item()";
+              "<C-k>" = "cmp.mapping.select_prev_item()";
+              "<C-e>" = "cmp.mapping.abort()";
+              "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<C-CR>" = "cmp.mapping.confirm({ select = true })";
+              "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+            };
+          };
+        };
+    cmp-nvim-lsp = {enable = true;}; # lsp
+    cmp-buffer = {enable = true;};
+    cmp-path = {enable = true;}; # file system paths
+    cmp_luasnip = {enable = true;}; # snippets
+    cmp-cmdline = {enable = false;}; # autocomplete for cmdline      
     };
 };
   programs.kitty.enable = true;
