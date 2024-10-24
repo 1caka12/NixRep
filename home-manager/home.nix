a32d49b540d4eb3b70fdc90e9f2f4bbcd3a566e7
Ivo <i.matsaliev@student.vu.nl>
2024-10-24 22:24:45 +0200

    Separated nixvim successfully

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,6 +33,7 @@
   ];
     imports = [
         ./nixvim
+        ./tmux
     ];
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
@@ -50,23 +51,7 @@
   programs.fish = {
     enable = true;
   };
-  programs.tmux = {
-    enable = true;
-    plugins = with pkgs;[
-        tmuxPlugins.catppuccin
-        tmuxPlugins.battery
-        tmuxPlugins.better-mouse-mode
-    ];
-    extraConfig = "
-        unbind C-b
-        set-option -g prefix C-Space
-        bind-key C-Space send-prefix
-        set -g status-position top
-        set -sg escape-time 0
-        set -g base-index 1
-    ";
-  };
-  programs.firefox.enable = true;
+    programs.firefox.enable = true;
   programs.starship = {
     enable = true;
     settings = {
---
+++ home-manager/tmux/default.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}:
+{
+ programs.tmux = {
+    enable = true;
+    plugins = with pkgs;[
+        tmuxPlugins.catppuccin
+        tmuxPlugins.battery
+        tmuxPlugins.better-mouse-mode
+    ];
+    extraConfig = "
+        unbind C-b
+        set-option -g prefix C-Space
+        bind-key C-Space send-prefix
+        set -g status-position top
+        set -sg escape-time 0
+        set -g base-index 1
+    ";
+  };
+}
