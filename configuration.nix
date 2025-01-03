3afbc2a5166719634142a5bad244c976f8fa9f14
Ivo <i.matsaliev@student.vu.nl>
2025-01-03 15:44:04 +0100

    Working on pipewire

--- configuration.nix
+++ configuration.nix
@@ -132,11 +132,15 @@
       };
   };
 
-  # Screenshare + audioshare
+  # Screenshare + audioshare + audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
-    jack.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    wireplumber.enable = true;
   };
 
   # Light stuff
